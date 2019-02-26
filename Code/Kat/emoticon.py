c241c1e2fc08df35f901ea641487062fc5fa4684
Mary Ritter <Mary Ritter@Bill-VaioZ.sci.foodtrak.com>
2019-02-26 10:53:04 -0800

    I'm changing stuff

--- Code/Kat/emoticon.py
+++
@@ -1,8 +0,0 @@
-import random
-eyes_list = [':', ';', '8']
-nose_list = ['3', '-', '>']
-mouth_list = [')', '(', '|']
-out_string = ''
-for num in range(5):
-    out_string = out_string + random.choice(eyes_list) + random.choice(nose_list) + random.choice(mouth_list) 
-print(out_string)
