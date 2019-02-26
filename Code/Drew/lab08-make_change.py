16601cbcf0fc1927298482b7f7a5aca9716ec7a3
Drew Herron <herron@mailbox.org>
2019-02-26 15:00:17 -0800

    testing

--- Code/Drew/lab08-make_change.py
+++ Code/Drew/lab08-make_change.py
@@ -5,17 +5,17 @@ coins = [
     ('penny', 1)
 ]
 coins = dict(coins)
-wallet = input('Enter an amount in dollars:\n$')
-wallet = int(wallet)
+wallet = float(input('Enter an amount in dollars:\n$'))
+wallet = float(wallet * 100)
 quarters = wallet // coins['quarter']
-print(str(quarters)+' quarters')
+print(str(int(quarters))+' quarters')
 wallet %= coins['quarter']
 dimes = wallet // coins['dime']
-print(str(dimes)+' dimes')
+print(str(int(dimes))+' dimes')
 wallet %= coins['dime']
 nickels = wallet // coins['nickel']
-print(str(nickels)+' nickels')
+print(str(int(nickels))+' nickels')
 wallet %= coins['nickel']
 pennies = wallet // coins['penny']
-print(str(pennies)+' pennies')
+print(str(int(pennies))+' pennies')
 wallet %= coins['penny']
