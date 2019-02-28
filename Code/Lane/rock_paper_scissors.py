f99ff9d9e9fb8bfb4427aefdb29a2599a0e71317
Your Name <you@example.com>
2019-02-28 14:26:23 -0800

    Uploading lab files

--- Code/Lane/rock_paper_scissors.py
+++ Code/Lane/rock_paper_scissors.py
@@ -1,7 +1,5 @@
 import random
-# Let's play rock-paper-scissors with the computer.
 
-# The computer will ask the user for their choice (rock, paper, scissors)
 game_running = True
 while game_running:
     choices = ['r', 'p', 's']
@@ -10,15 +8,11 @@ while game_running:
     while player_choice not in choices:
         player_choice = input('Choose (R)ock (P)aper or (S)cissors: ').lower()
 
-    # The computer will randomly choose rock, paper or scissors
     computer_choice = random.choice(choices)
-
-    # Determine who won and tell the user
-    # Let's list all the cases:
 
     # rock vs rock (tie)
     print('Player has a {}.'.format(player_choice))
-    print('Player has a {}.'.format(computer_choice))
+    print('Computer has a {}.'.format(computer_choice))
 
     if player_choice == computer_choice:
         print('It\'s a tie!')
---
+++ Code/Lane/unit_converter_v1.py
@@ -0,0 +1,6 @@
+# unit converter Lab 9 version 1
+
+
+user_ft = float(input("What is the distance in feet?: "))
+meters_in_feet = user_ft * .3048
+print(f"{user_ft} ft is {meters_in_feet} m")
