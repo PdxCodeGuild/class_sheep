049bd98068a0bf6596c7e6801e8c59d32be615c1
Mary Ritter <Mary Ritter@Bill-VaioZ.sci.foodtrak.com>
2019-03-01 12:06:12 -0800

    adding lab 13

---
+++ Code/Kat/lab13.py
@@ -0,0 +1,16 @@
+#ROT 13
+import string
+user_letters = list(string.ascii_lowercase)
+to_encode = input("Type a word that you would like to encode >")
+out_string = ''
+for character in to_encode:
+    if character in string.ascii_letters:
+        out_string += character
+    else:
+        out_string += ''
+for letter in out_string:
+    letter = letter.lower()
+    direction = user_letters.index(letter)
+    direction += 13
+    print(f"{user_letters[direction % 26]}", end='')
+print()
