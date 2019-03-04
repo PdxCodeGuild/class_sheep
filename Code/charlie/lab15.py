9b751070d3cb6da6c69a749e865d479d070769c1
Charles Bush <charlie@Ramya-PC.localdomain>
2019-03-04 13:47:00 -0800

    roman numerals

--- Code/charlie/lab15.py
+++ Code/charlie/lab15.py
@@ -1,60 +1,66 @@
 #number_to_phrase
+
+
+
+
 import string
-hundreds_dict = {100: 'One Hundred',200: 'Two Hundred',300: 'Three Hundred',400: 'Four Hundred',500: 'Five Hundred',600: 'Six Hundred',700: 'One Hundred',800: 'Eight Hundred',900: 'Nine Hundred'}
-tens_dict = {10: 'Ten', 20: 'Twenty', 30:'Thirty', 40:'Forty', 50:'Fifty', 60: 'Sixty', 70:'Seventy', 80: 'Eighty', 90: 'Ninety'}
-singles_dict = {1: 'one', 2: 'two', 3:'three', 4:'four', 5:'five', 6:'six', 7:'seven', 8:'eight', 9:'nine'}
+
+def teen(in_num):
+    out_string = ''
+    out_string += teen_dict[user_digit]
+    print(out_string)
+    return(out_string)
+
+def singles(in_num):
+    user_digit_singles = user_digit % 10
+    out_string = ''
+    out_string += singles_dict[user_digit_singles]
+    print(out_string)
+    return(out_string)
+
+def tens(in_num):
+    user_digit_tens = (user_digit // 10)*10
+    user_digit_singles = user_digit % 10
+    out_string = ''
+    out_string += tens_dict[user_digit_tens] + '-' + singles_dict[user_digit_singles]
+    print(out_string)
+    return(out_string)
+
+def hundreds(in_num):
+    user_digit_hundreds =  (user_digit //100)*100
+    user_digit_tens = ((user_digit // 10)*10) - user_digit_hundreds
+    user_digit_singles = user_digit % 10
+    out_string = ''
+    out_string += hundreds_dict[user_digit_hundreds] +' and '+ tens_dict[user_digit_tens] + '-' + singles_dict[user_digit_singles]
+    print(out_string)
+    return(out_string)
+
+# V1,2 dict
+# hundreds_dict = {100: 'One Hundred',200: 'Two Hundred',300: 'Three Hundred',400: 'Four Hundred',500: 'Five Hundred',600: 'Six Hundred',700: 'One Hundred',800: 'Eight Hundred',900: 'Nine Hundred'}
+# tens_dict = {10: 'Ten', 20: 'Twenty', 30:'Thirty', 40:'Forty', 50:'Fifty', 60: 'Sixty', 70:'Seventy', 80: 'Eighty', 90: 'Ninety'}
+# singles_dict = {1: 'one', 2: 'two', 3:'three', 4:'four', 5:'five', 6:'six', 7:'seven', 8:'eight', 9:'nine'}
+# teen_dict = {11: 'eleven', 12: 'Twelve', 13: 'Thirtenn', 14: 'Fourteen', 15: 'Fifteen', 16: 'Sixteen', 17: 'Seventeen', 18: 'Eighteen', 19: 'Nineteen'}
+
+#V 3 dict
+hundreds_dict = {100: 'C',200: 'CC',300: 'CCC',400: 'CD',500: 'D',600: 'DC',700: 'DCC',800: 'DCCC',900: 'CM'}
+tens_dict = {10: 'X', 20: 'XX', 30:'XXX', 40:'XL', 50:'L', 60: 'LX', 70:'LXX', 80: 'LXXX', 90: 'XC'}
+singles_dict = {1: 'I', 2: 'II', 3:'II', 4:'IV', 5:'V', 6:'VI', 7:'VII', 8:'VIII', 9:'IX'}
 user_digit = int(input("enter digit "))
 
-if user_digit == 11:
-    print('Eleven')
-elif user_digit == 12:
-    print('Twelve')
-elif user_digit == 13:
-    print('Thirteen')
-elif user_digit == 14:
-    print('Fourteen')
-elif user_digit == 15:
-    print('Fifteen')
-elif user_digit == 16:
-    print('Sixteen')
-elif user_digit == 17:
-    print('Seventeen')
-elif user_digit == 18:
-    print('Eighteen')
-elif user_digit == 19:
-    print('Nineteen')
 
-# while user_digit >= 20 or user_digit <= 10:
+if user_digit >=11 and user_digit <=19:
+    teen(user_digit)
+
 while user_digit >= 20 or user_digit <= 10:
     if user_digit <= 10:
-
-        user_digit_singles = user_digit % 10
-        print(user_digit_singles)
-        out_string = ''
-        out_string += singles_dict[user_digit_singles]
-        print(out_string)
+        singles(user_digit)
         break
-
-
     if user_digit >= 20 and user_digit <= 99:
-        user_digit_tens = (user_digit // 10)*10
-        print(user_digit_tens)
-        user_digit_singles = user_digit % 10
-        print(user_digit_singles)
-        out_string = ''
-        out_string += tens_dict[user_digit_tens] + '-' + singles_dict[user_digit_singles]
-        print(out_string)
+        tens(user_digit)
         break
-
     if user_digit >= 100:
-        user_digit_hundreds =  (user_digit //100)*100
-        print(user_digit_hundreds)
-        user_digit_tens = ((user_digit // 10)*10) - user_digit_hundreds
-        print(user_digit_tens)
-        user_digit_singles = user_digit % 10
-        print(user_digit_singles)
-
-        out_string = ''
-        out_string += hundreds_dict[user_digit_hundreds] +' and '+ tens_dict[user_digit_tens] + '-' + singles_dict[user_digit_singles]
-        print(out_string)
+        hundreds(user_digit)
         break
+
+
+# #ver1
