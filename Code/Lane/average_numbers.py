d9ace2a993254041c251f3edbd435bc7f90a29c7
Your Name <you@example.com>
2019-03-07 15:13:53 -0800

    Updating file

--- Code/Lane/average_numbers.py
+++ Code/Lane/average_numbers.py
@@ -1,3 +1,5 @@
+version 1
+
 nums = [5, 0, 8, 3, 4, 1, 6]
 counter = 0
 for n in nums:
@@ -7,17 +9,37 @@ average = counter/len(nums)
 print('The average of these numbers is: ', average)
 
 
-
-#version 2
-
-user_numbers = []
-
-while True:
-
-    user_input = input("Enter a number to continue or done to quit: ")
-    if user_input == 'done':
-        break
-    user_input = int(user_input)
-    user_numbers.append(user_input)
-
-print(sum(user_numbers) / len(user_numbers))
+#
+# #version 2
+#
+# user_numbers = []
+#
+# while True:
+#
+#     user_input = input("Enter a number to continue or done to quit: ")
+#     if user_input == 'done':
+#         break
+#     user_input = int(user_input)
+#     user_numbers.append(user_input)
+#
+# print("average = ", sum(user_numbers) / len(user_numbers))
+#
+#
+# #version 3
+# nums = []
+#
+# while True:
+#     user_input = input("Enter a number to continue or done to quit: ")
+#     if user_input == 'done':
+#         break
+#     else:
+#         user_input = int(user_input)
+#         nums.append(user_input)
+#
+# length = len(nums)
+# nums.sort()
+#
+# if length % 2 == 0:
+#     print(nums[len(nums) // 2 - 1], nums[len(nums) // 2])
+# else:
+#     print(nums[len(nums) // 2])
