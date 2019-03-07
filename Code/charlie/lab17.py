286b96f32aadeb0c5d1d706ee9c5ff5db9e0dcf5
Charles Bush <charlie@Ramya-PC.localdomain>
2019-03-07 15:32:13 -0800

    lab17.py

---
+++ Code/charlie/lab17.py
@@ -0,0 +1,46 @@
+#lab17.py
+import string
+def check_palindrome(input_string):
+    abc_list = list(string.ascii_lowercase)
+    list2 = []
+    string_list = []
+    reverse_list = []
+    for letter in input_string:
+        string_list.append(abc_list.index(letter))
+    for letter in input_string:
+        reverse_list.append(abc_list.index(letter))
+
+    reverse_list.reverse()
+
+    print(string_list)
+    print(reverse_list)
+    index = 0
+    length = len(string_list)
+    for index in range(len(string_list)):
+        if string_list[index] == reverse_list[index]:
+            index +=1
+            if index == length:
+                return True
+        else:
+            return False
+    #     for letter2 in range(len(reverse_list)):
+    #         if current_index == letter2[index]:
+    #
+    #         return False
+
+        # reverse_list=reverse_list.index(letters)
+        # print(f"index:{letter}")
+        # print(f"letter:{letter}")
+
+
+
+
+print(check_palindrome('racecar'))
+
+
+''' i want to input a string
+then i want to convert the string into indicies
+then i want to compare the first and last indicies to determine if they are the same
+if they are the same, return true
+else return false
+'''
