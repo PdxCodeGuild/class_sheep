46b1c5f65bb5b7f9057e35a236da6d7f5fee1675
Drew Herron <herron@mailbox.org>
2019-03-07 12:26:51 -0800

    image color sorter

---
+++ Code/Drew/lab16-image_manipulation.py
@@ -0,0 +1,25 @@
+from PIL import Image, ImageDraw
+
+#import image
+img = Image.open("rainbow.jpg") # must be in same folder
+width, height = img.size
+pixels = img.load()
+
+#get colors
+colors = []
+for i in range(width):
+    for j in range(height):
+        r, g, b = pixels[i, j]
+        colors.append((r, g, b))
+
+#sort and draw colors
+colors.sort()
+img = Image.new('RGB', (width, height))
+draw = ImageDraw.Draw(img)
+c = 0
+for i in range(width):
+    for j in range(height):
+        draw.point((i, j), fill=colors[c])
+        c += 1
+
+img.show()
