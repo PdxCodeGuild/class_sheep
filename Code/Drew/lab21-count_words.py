1c63d1f209de4e4ad3cd0ffb4bbd854d7e9503be
Drew Herron <herron@mailbox.org>
2019-03-13 16:19:07 -0700

    UTF-8 error was an Atom thing?

--- Code/Drew/lab21-count_words.py
+++ Code/Drew/lab21-count_words.py
@@ -28,5 +28,5 @@ for i in words:
 words = list(word_dict.items())
 words.sort(key=lambda tup: tup[1], reverse=True)
 
-for i in range(min(10, len(words))):
+for i in range(min(15, len(words))):
     print(words[i])
