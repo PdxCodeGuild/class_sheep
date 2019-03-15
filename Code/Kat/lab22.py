0bf8b970c2e5c751c0543d850ac477bfb7643484
Mary Ritter <Mary Ritter@Bill-VaioZ.sci.foodtrak.com>
2019-03-15 16:16:40 -0700

    lab 22

---
+++ Code/Kat/lab22.py
@@ -0,0 +1,57 @@
+import requests
+import math
+
+response = requests.get('http://www.gutenberg.org/files/84/84-0.txt')
+text = response.text
+text = text.replace('  ', ' ')
+
+import string
+
+def count_characters(text):
+    character_counter = 0
+    for character in text:
+        if character in string.ascii_letters or character in string.digits:
+            character_counter += 1
+    return character_counter
+
+
+def count_words(text):
+    text = text.replace('—', ' ')
+    text = text.replace('-', ' ')
+    text = text.split()
+    return len(text)
+
+
+def count_sentences(text):
+    text = text.replace('...', '.')
+    sentence_counter = 0
+    for character in text:
+        if character == '.' or character == '!' or character == '?':
+            sentence_counter += 1
+    return sentence_counter
+
+def find_ari(text):
+    first_step = 4.71 * (count_characters(text)/count_words(text))
+    second_step = 0.5 * (count_words(text)/count_sentences(text)) - 21.43
+    return math.ceil(first_step + second_step)
+
+ari_scale = {
+     1: {'ages':   '5-6', 'grade_level': 'Kindergarten'},
+     2: {'ages':   '6-7', 'grade_level':    '1st Grade'},
+     3: {'ages':   '7-8', 'grade_level':    '2nd Grade'},
+     4: {'ages':   '8-9', 'grade_level':    '3rd Grade'},
+     5: {'ages':  '9-10', 'grade_level':    '4th Grade'},
+     6: {'ages': '10-11', 'grade_level':    '5th Grade'},
+     7: {'ages': '11-12', 'grade_level':    '6th Grade'},
+     8: {'ages': '12-13', 'grade_level':    '7th Grade'},
+     9: {'ages': '13-14', 'grade_level':    '8th Grade'},
+    10: {'ages': '14-15', 'grade_level':    '9th Grade'},
+    11: {'ages': '15-16', 'grade_level':   '10th Grade'},
+    12: {'ages': '16-17', 'grade_level':   '11th Grade'},
+    13: {'ages': '17-18', 'grade_level':   '12th Grade'},
+    14: {'ages': '18-22', 'grade_level':      'College'}
+}
+
+demographics = ari_scale[find_ari(text)]
+
+print(f"The ARI for Mary Shelley's Frankenstein is {find_ari(text)}. This corresponds to ages {demographics['ages']} or {demographics['grade_level']}.")
