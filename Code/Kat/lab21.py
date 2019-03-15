6d55b0001327585bdef2c0c2cfbd92a7a3e68fcf
Mary Ritter <Mary Ritter@Bill-VaioZ.sci.foodtrak.com>
2019-03-15 12:12:03 -0700

    lab 21 v 3

--- Code/Kat/lab21.py
+++ Code/Kat/lab21.py
@@ -1,6 +1,9 @@
 # count_words
 import requests
 import string
+#function to replace lambda in provided sort code
+def return_index_one(in_tuple):
+    return in_tuple[1]
 # # acquiring text using requests
 response = requests.get('http://www.gutenberg.org/files/84/84-0.txt')
 text = response.text
@@ -38,39 +41,38 @@ for word in word_list:
         word_count_dict[word] += 1
     else:
         word_count_dict[word] = 1
-# word_dict is a dictionary where the key is the word and the value is the count
-# words = list(word_count_dict.items()) # .items() returns a list of tuples
-# words.sort(key=lambda tup: tup[1], reverse=True)  # sort largest to smallest, based on count
-# for i in range(min(10, len(words))):  # print the top 10 words, or all of them, whichever is smaller
-#     print(words[i])
+# sort words and and print10  most common words
+words = list(word_count_dict.items()) # .items() returns a list of tuples
+words.sort(key=return_index_one, reverse=True)  # sort largest to smallest, based on count
+for i in range(min(10, len(words))):  # print the top 10 words, or all of them, whichever is smaller
+    print(words[i])
+
+
 
 #Version 2, counting word pairs
 word_pair_dict = {}
 for i in range(len(word_list) - 1):
-    word_pair = word_list[i] + ' ' + word_list[i + 1]
+    word_pair = (word_list[i], word_list[i + 1])
     if word_pair in word_pair_dict:
         word_pair_dict[word_pair] += 1
     else:
         word_pair_dict[word_pair] = 1
+#sort pairs and print 10 most common pairs
+pairs = list(word_pair_dict.items()) # .items() returns a list of tuples
+pairs.sort(key=return_index_one, reverse=True)  # sort largest to smallest, based on count
+for i in range(min(10, len(pairs))):  # print the top 10 words, or all of them, whichever is smaller
+    print(pairs[i])
 
-# pairs = list(word_pair_dict.items()) # .items() returns a list of tuples
-# pairs.sort(key=lambda tup: tup[1], reverse=True)  # sort largest to smallest, based on count
-# for i in range(min(10, len(pairs))):  # print the top 10 words, or all of them, whichever is smaller
-#     print(pairs[i])
 
+#Version 3
+user_pair_dict = {}
 user_input = input("What word would you like to check? ")
-choice_pair_dict = {}
-choice_index_list = []
-if user_input in word_list:
-    choice_index_list += word_list[user_input]
-# for i in range(len(word_list) - 1):
-#     if user_input in word_list:
-#         choice_index_list.append(user_input[i])
-print (choice_index_list)
-
-# for i in word_list:
-#     if user_input in word_list:
-#         choice_index_list += i
-# print(choice_index_list)
-# for choice in word_list:
-#     choice_pair = word_list[user_input] + ' ' + word_list[user_input + 1]
+#Check if user word is in word_pair_dict
+for item in word_pair_dict.keys():
+    if user_input == item[0]:
+        user_pair_dict[item] = word_pair_dict[item]
+#Count the number of occurrences and print top 10 or most common
+user_pairs = list(user_pair_dict.items()) # .items() returns a list of tuples
+user_pairs.sort(key=return_index_one, reverse=True)  # sort largest to smallest, based on count
+for i in range(min(10, len(user_pairs))):  # print the top 10 words, or all of them, whichever is smaller
+    print(user_pairs[i])
