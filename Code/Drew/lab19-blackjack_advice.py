994caaf55697466f8426ce14b5e4cc8b4e8a1558
Drew Herron <herron@mailbox.org>
2019-03-19 19:44:22 -0700

    fixing ace problems

--- Code/Drew/lab19-blackjack_advice.py
+++ Code/Drew/lab19-blackjack_advice.py
@@ -19,6 +19,8 @@ def get_card():
 potential_list = []
 user_hand = []
 points = 0
+blackjack = False
+bust = False
 
 # Two cards for first deal
 user_hand.append(get_card())
@@ -31,20 +33,22 @@ if 'A' in user_hand:
     temp_hand.append('B')
     potential_list.append(temp_hand)
 print(f"You have {potential_list[0]}.")
+
+# Check if any hand = 21
 points = check_points(user_hand)
 if len(potential_list) > 1:
     for hand in potential_list:
         if check_points(hand) == 21:
             print("Blackjack!")
-            break
-        else:
-            print("With aces being 1 or 11 points,")
-            for hand in potential_list:
-                print(f"You could have {check_points(hand)} points.")
+            blackjack = True
+    if blackjack == False:
+        print("With aces being 1 or 11 points,")
+        for hand in potential_list:
+            print(f"You could have {check_points(hand)} points.")
 else:
     print(f"You have {points} points.")
 
-while True:
+while blackjack == False:
     hit = input("(h)it or (s)tay?\n")
     if hit == 'h':
         new_card = get_card()
@@ -55,15 +59,15 @@ while True:
             temp_hand = potential_list[-1][:]
             temp_hand.append('B')
             potential_list.append(temp_hand)
-        if len(potential_list) > 1:
+    if len(potential_list) > 1:
+        for hand in potential_list:
+            if check_points(hand) == 21:
+                print("Blackjack!")
+                blackjack = True
+        if blackjack == False:
+            print("With aces being 1 or 11 points,")
             for hand in potential_list:
-                if check_points(hand) == 21:
-                    print("Blackjack!")
-                    break
-                else:
-                    print("With aces being 1 or 11 points,")
-                    for hand in potential_list:
-                        print(f"You could have {check_points(hand)} points.")
+                print(f"You could have {check_points(hand)} points.")
         print(user_hand)
         print(potential_list)
         continue
