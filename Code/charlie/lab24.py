ab60d7c82972fca13dab23c4b5a5a4fc3beb82ac
Charles Bush <charlie@Ramya-PC.localdomain>
2019-03-22 16:33:52 -0700

    lab24.py

--- Code/charlie/lab24.py
+++ Code/charlie/lab24.py
@@ -46,37 +46,105 @@ for i in range(len(master_list)):
         day = (master_list[i]['day'])
 
 #I want to determine the total rainfall per year.  To do this, I created two empty lists, one for year and one for rain.  i set rain = to 0.  i iterate over the master_list.  In the next step, i have an if statement  and i compare the years to each other.  as long as the current indexs year is equal to the next indexs year, the rain is summed up.  once the index points to a differet year, rain is appended to the rain list, the year is appended to the year list and the counter is set to zero.  the loop will continue for each year.
-year_list = []
+years = []
 rain_list = []
 rain = 0
-for i in range(len(master_list)- 1):
-    # print(master_list[i]['day'].year)
-    if master_list[i]['day'].year == master_list[i+1]['day'].year:
-        rain += (master_list[i]['rainfall'])
-    else:
-        year_list.append(master_list[i]['day'].year)
+for i in range(len(master_list)):
+    rain += master_list[i]['rainfall']
+    if i == len(master_list)-1 or master_list[i]['day'].year != master_list[i+1]['day'].year:
+        years.append(master_list[i]['day'].year)
         rain_list.append(rain)
         rain = 0
+#===================ALTERNATIVE=================================
+# years = []
+# for i in range(len(master_list)):
+#     year = master_list[i]['day'].year
+#     if year not in years:
+#         years.append(year)
+# years.sort()
+#
+# totals = []
+# for i in range(len(years)):
+#     totals.append(0)
+#
+# for i in range(len(years)):
+#     for j in range(len(master_list)):
+#         if years[i] == master_list[i]['day'].year:
+#             totals[i] += master_list[i]['rainfall']
 
-#I want to find the max rainfall.  To do this, i set two max rain to zero. i iterate over the rain_list and for each indicie i compare it to the max rain.  if it's larger, i set the larger rainfall to the max_annual_rain.  I use the same indicie in the year_list to determine what year had the max rainfall.  i point max_year at that.
 
-max_year = 0
-max_annual_rain = 0
-for i in range(len(rain_list)):
-    if rain_list[i] > max_annual_rain:
-        max_annual_rain = rain_list[i]
-        max_year = year_list[i]
+#============================ANNUAL RAINFALL==================================
+##I want to find the max rainfall.  To do this, i set two max rain to zero. i iterate over the rain_list and for each indicie i compare it to the max rain.  if it's larger, i set the larger rainfall to the max_annual_rain.  I use the same indicie in the years to determine what year had the max rainfall.  i point max_year at that.
 
-for i in range(len(rain_list)):
-    rain_list[i] /= 100
+# max_year = 0
+# max_annual_rain = 0
+# for i in range(len(rain_list)):
+#     if rain_list[i] > max_annual_rain:
+#         max_annual_rain = rain_list[i]
+#         max_year = years[i]
+#
+# for i in range(len(rain_list)):
+#     rain_list[i] /= 100
+#
+# print(years)
+# print(rain_list)
+# print(f"The year with the most water, {max_annual_rain} was {max_year}")
+#
+# plt.plot(years, rain_list, 'rs')
+# plt.title('Annual precipitaion for Swan Island, OR (2007 to 2019)')
+# plt.xlabel('Years')
+# plt.ylabel('Annual Rainfall (inches)')
+# plt.show()
 
-print(year_list)
-print(rain_list)
-print(f"The year with the most water, {max_annual_rain} was {max_year}")
+#======================MONTHLY RAINFALL=======================================
+##Generate a graph displaying monthly rain data over the duration of the data
 
-plt.plot(year_list, rain_list, 'ro')
-plt.title('Annual precipitaion for Swan Island, OR (2008 to 2019)')
-plt.xlabel('Years')
+#Take the master data and convert the date into month, year tuple.  i append each individual tuple to the list
+month_years_tuple_list = []
+for i in range(len(master_list)):
+    tuple = (master_list[i]['day'].month, master_list[i]['day'].year)
+    if tuple not in month_years_tuple_list:
+        month_years_tuple_list.append(tuple)
+
+#try to determine the total rainfall per month.  we create a list that contains the same number of iterations as the tuple list.
+totals = []
+for i in range(len(month_years_tuple_list)):
+    totals.append(0)
+
+#for each iteration, we iterate through the master_list.  if the month value is equal to the month value in both lists, we add the rainfall data at the jth index to the totals list at the ith index.
+for i in range(1, len(month_years_tuple_list)):
+    for j in range(len(master_list)):
+        if month_years_tuple_list[i][0] == master_list[j]['day'].month:
+            if month_years_tuple_list[i][1] == master_list[j]['day'].year:
+                totals[i] += master_list[j]['rainfall']
+
+
+
+months = []
+years = []
+for i in range(len(month_years_tuple_list)):
+    month, year = month_years_tuple_list[i]
+    months.append(month)
+    years.append(year)
+
+months = []
+years = []
+month_years_string_list = []
+for i in range(len(month_years_tuple_list)):
+    month_years_tuple_list[i] = str(month_years_tuple_list[i])
+    month_years_string_list.append(month_years_tuple_list[i])
+
+for i in range(len(month_years_string_list)):
+    month_years_string_list[i].strip('()')
+
+for i in range(len(totals)):
+    totals[i] /= 100
+# plt.plot(months, totals, 'ro')
+plt.title('Monthly precipitaion for Swan Island, OR (2007 to 2019)')
+plt.plot(month_years_string_list, totals)
+plt.xlabel('Months')
 plt.ylabel('Annual Rainfall (inches)')
 plt.show()
-exit()
+
+print(totals)
+# print(month_years_string_list)
