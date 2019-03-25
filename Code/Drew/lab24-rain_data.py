993c98046d63dfef05885e98587e0d88efd70bf1
Drew Herron <herron@mailbox.org>
2019-03-25 14:51:58 -0700

    replaced this with v2, deleted v2

--- Code/Drew/lab24-rain_data.py
+++ Code/Drew/lab24-rain_data.py
@@ -1,6 +1,7 @@
 import requests
 import re
 import datetime
+import matplotlib.pyplot as plt
 
 # Get data
 r = requests.get('https://or.water.usgs.gov/non-usgs/bes/pcc_sylvania.rain')
@@ -74,43 +75,57 @@ for i in range(1, 13):
     for tup in rain_record:
         if tup.month == 12:
             dec_list.append(rain_record[tup])
-            #print(rain_record[tup])
+
+x_values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+y_values = []
 
 jan_rains = sum(jan_list)
 jan_avg = jan_rains/len(jan_list)
+y_values.append(jan_avg)
 
 feb_rains = sum(feb_list)
 feb_avg = feb_rains/len(feb_list)
+y_values.append(feb_avg)
 
 mar_rains = sum(mar_list)
 mar_avg = mar_rains/len(mar_list)
+y_values.append(mar_avg)
 
 apr_rains = sum(apr_list)
 apr_avg = apr_rains/len(apr_list)
+y_values.append(apr_avg)
 
 may_rains = sum(may_list)
 may_avg = may_rains/len(may_list)
+y_values.append(may_avg)
 
 jun_rains = sum(jun_list)
 jun_avg = jun_rains/len(jun_list)
+y_values.append(jun_avg)
 
 jul_rains = sum(jul_list)
 jul_avg = jul_rains/len(jul_list)
+y_values.append(jul_avg)
 
 aug_rains = sum(aug_list)
 aug_avg = aug_rains/len(aug_list)
+y_values.append(aug_avg)
 
 sep_rains = sum(sep_list)
 sep_avg = sep_rains/len(sep_list)
+y_values.append(sep_avg)
 
 oct_rains = sum(oct_list)
 oct_avg = oct_rains/len(oct_list)
+y_values.append(oct_avg)
 
 nov_rains = sum(nov_list)
 nov_avg = nov_rains/len(nov_list)
+y_values.append(nov_avg)
 
 dec_rains = sum(dec_list)
 dec_avg = dec_rains/len(dec_list)
+y_values.append(dec_avg)
 
 print()
 print("Average Rainfall per Month:")
@@ -126,3 +141,7 @@ print(f"Sep - {sep_avg}")
 print(f"Oct - {oct_avg}")
 print(f"Nov - {nov_avg}")
 print(f"Dec - {dec_avg}")
+
+plt.plot(x_values, y_values)
+plt.gcf().autofmt_xdate()
+plt.show()
