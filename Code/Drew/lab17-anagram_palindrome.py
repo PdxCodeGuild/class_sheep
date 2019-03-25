ba0b95f73d5983e5236b1ca70627c25aeda4bd37
Drew Herron <herron@mailbox.org>
2019-03-25 14:47:30 -0700

    added comments

--- Code/Drew/lab17-anagram_palindrome.py
+++ Code/Drew/lab17-anagram_palindrome.py
@@ -1,12 +1,13 @@
-
+# Function to make a backward copy of string and compare with the original (as lists)
 def palindrome_check(var):
     fwd = []
     bwd = []
-    for x in range(len(var)):
-        fwd.append(var[x])
-        bwd.append(var[(len(var)-1)-x])
+    for i in range(len(var)):
+        fwd.append(var[i])
+        bwd.append(var[(len(var)-1)-i])
     return fwd == bwd
 
+# Function to copy the string, sort both, and compare
 def anagram_check(a, b):
     a = a.lower()
     b = b.lower()
@@ -14,8 +15,10 @@ def anagram_check(a, b):
     b = b.replace(' ','')
     return ''.join(sorted(a)) == ''.join(sorted(b))
 
+# Get user to choose between anagram and palindrome
 func_choice = input("Type 'a' for anagram check, 'p' for palindrome check:\n")
 
+# Apply the appropriate function:
 if func_choice == 'a':
     user_string1 = input("Enter a word or phrase:\n")
     user_string2 = input("Enter another word or phrase:\n")
@@ -23,7 +26,6 @@ if func_choice == 'a':
         print("These are anagrams.")
     else:
         print("These are not anagrams.")
-
 if func_choice == 'p':
     user_string = input("Enter a string:\n")
     if palindrome_check(user_string) == True:
