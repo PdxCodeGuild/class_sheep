76e17556ad8ec4e73ff1dc91d6fa87760beb24e7
Charles Bush <bushc663@gmail.com>
2019-03-26 08:25:36 -0700

    lab8v3.py

--- Code/charlie/lab8v3.py
+++ Code/charlie/lab8v3.py
@@ -3,32 +3,34 @@
 
 
 #i want to allow the user to create custom coin values.
-user_coins = ['quarter', 'dime', 'nickel', 'penny']
+coins = ['quarter', 'dime', 'nickel', 'penny']
 
-coin_values = []
+values = []
 coin_value = 0
-for i in range(len(user_coins)):
-    coin_value = int(input(f'How much do you want a {user_coins[i]} to be worth?\n:'))
-    coin_values.append(coin_value)
+for i in range(len(coins)):
+    coin_value = int(input(f'How much do you want a {coins[i]} to be worth?\n:'))
+    values.append(coin_value)
 
-#I want to create a dictionary of custom coins, and then sort them from largest to smallest
-custom_coins = {}
-for i in range(len(user_coins)):
-    if user_coins[i] not in custom_coins:
-        custom_coins[user_coins[i]] = coin_values[i]
-sorted(custom_coins, reverse=True)
-print(custom_coins)
+#Create a dictionary of custom coins, and then sort them from largest to smallest
+coin_dictionary = {}
+for i in range(len(coins)):
+    if coins[i] not in coin_dictionary:
+        coin_dictionary[coins[i]] = values[i]
+sorted(coin_dictionary, reverse=True)
+print(coin_dictionary)
 
+#convert user change into the custom amount of pennies
 user_change = float(input('how much money do you have?'))
-total_pennies = round(user_change / custom_coins['penny'])
+total_pennies = round(user_change / coin_dictionary['penny'])
 print(total_pennies)
 
-quarters = total_pennies // custom_coins['quarter']
-remainder = total_pennies % custom_coins['quarter']
-dimes = remainder // custom_coins['dime']
-remainder_step2 = remainder % custom_coins['dime']
-nickels = remainder_step2 // custom_coins['nickel']
-pennies = remainder_step2 % custom_coins['nickel']
+#use total pennies to determine how many of the larger coins we have
+quarters = total_pennies // coin_dictionary['quarter']
+remainder = total_pennies % coin_dictionary['quarter']
+dimes = remainder // coin_dictionary['dime']
+remainder_step2 = remainder % coin_dictionary['dime']
+nickels = remainder_step2 // coin_dictionary['nickel']
+pennies = remainder_step2 % coin_dictionary['nickel']
 
 
 print(f'quarters {quarters}, dimes {dimes}, nickels {nickels}, pennies {pennies}')
