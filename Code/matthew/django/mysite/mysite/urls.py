6957bbb17cdef0306aec195a94f25544f8b63f80
flux2341 <flux2341@gmail.com>
2019-05-08 14:44:55 -0700

    added ajax save data example

--- Code/matthew/django/mysite/mysite/urls.py
+++ Code/matthew/django/mysite/mysite/urls.py
@@ -11,4 +11,6 @@ urlpatterns = [
     path('contacts2/', include('contacts2.urls')),
     path('users/', include('users.urls')),
     path('fileapp/', include('fileapp.urls')),
+    path('githubbrowser/', include('githubbrowser.urls')),
+    path('ajax_demo/', include('ajax_demo.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
