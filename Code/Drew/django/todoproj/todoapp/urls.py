8800b756dd8508f95cfd1b4d6b813c48ae9685c4
Drew Herron <herron@mailbox.org>
2019-05-24 15:23:29 -0700

    Working on todo app

--- Code/Drew/django/todoproj/todoapp/urls.py
+++ Code/Drew/django/todoproj/todoapp/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 app_name = 'todoapp'
 urlpatterns = [
-    path('index/', views.index, name='index'),
+    path('', views.index, name='index'),
     path('save_todo/', views.save_todo, name='save_todo'),
+    path('complete_todo/', views.complete_todo, name='complete_todo'),
 ]
--- Code/Drew/django/todoproj/todoapp/views.py
+++ Code/Drew/django/todoproj/todoapp/views.py
@@ -1,19 +1,26 @@
 from django.shortcuts import render, reverse
 from django.http import HttpResponse, HttpResponseRedirect
 from .models import ToDoItem
-
-# def index(request):
-#     return HttpResponse('ok')
+from django.utils import timezone
 
 def index(request):
     context = {
-        'message': 'hello world!',
-        'todos': ToDoItem.objects.all()
+        'todos': ToDoItem.objects.filter(completed=False),
+        'completed': ToDoItem.objects.filter(completed=True)
     }
     return render(request, 'todoapp/index.html', context)
 
 def save_todo(request):
+    print(request.POST)
     todo_text = (request.POST['todo_text'])
-    todo_item = ToDoItem(text=todo_text)
+    todo_item = ToDoItem(todo_text=todo_text, completed_date=timezone.now(), completed=False)
+    todo_item.save()
+    return HttpResponseRedirect(reverse('todoapp:index'))
+
+def complete_todo(request):
+    print(request.POST)
+    todo_id = request.POST['todo_id']
+    todo_item = ToDoItem.objects.get(id=todo_id)
+    todo_item.completed = True
     todo_item.save()
     return HttpResponseRedirect(reverse('todoapp:index'))
